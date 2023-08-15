08ecffdd3cf642df19fc23e64908fb7591eb8e3a
heeju <heeju@imprimedicine.com>
2023-08-15 10:28:54 +0900

    seats

---
+++ alleat/src/Seat.js
@@ -0,0 +1,140 @@
+import { useLocation, useNavigate } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { Image, Modal, Button, Typography, Row, Col, InputNumber } from 'antd';
+import stores from './stores.js';
+import './Store.css';
+
+const { Title, Text } = Typography;
+
+export default function Seat() {
+    const location = useLocation();
+    const navigate = useNavigate();
+    const storeId = location.state?.storeId;
+    const store = stores[storeId];
+    const tables = store.tables;
+
+    const [modalVisible, setModalVisible] = useState(false);
+    const [adultCount, setAdultCount] = useState(0);
+    const [childCount, setChildCount] = useState(0);
+    const [tableNum, setTableNum] = useState(0);
+    const [tablesStatus, setTablesStatus] = useState([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]);
+
+    // const ws = new WebSocket("ws://172.30.1.55:8080");
+
+    // useEffect(() => {
+    //     ws.onopen = () => {
+    //       console.log('Connection is open.');
+    //       ws.send(JSON.stringify({ 'storeId': storeId, 'tableNum': -1, 'command': 'get' }));
+    //     };
+    
+    //     ws.onmessage = (event) => {
+    //       console.log('Received from server:', event.data);
+    
+    //       const jsonData = JSON.parse(event.data);
+    //       const newStatus = jsonData[1]['available'];
+    //       setTablesStatus(newStatus);
+    //     };
+    
+    //     ws.onerror = (error) => {
+    //       console.log('WebSocket error:', error);
+    //     };
+    
+    //     return () => {
+    //       ws.close();
+    //       console.log('Connection is closed.');
+    //     };
+    // }, [storeId, ws]);
+    
+    const getTables = () => {
+        return (
+          tables.map((tableRow, row) => (
+            <Row key={row} gutter={[10, 10]} style={{ flexDirection: 'row', width: '100%', justifyContent: 'space-around' }}>
+              {tableRow.map((table, col) => (
+                <Col key={row * 100 + col}>
+                  {table ?
+                    tablesStatus[table] ?
+                      <Button
+                        className="availableTable"
+                        onClick={() => { setModalVisible(!modalVisible); setTableNum(table); }}
+                      >
+                        좌석{table}
+                      </Button>
+                      :
+                      <Button
+                        className="reservedTable"
+                        onClick={() => { setTableNum(table); }}
+                      >
+                        좌석{table}
+                      </Button>
+                    :
+                    <div style={{height: '50px', width:'50px'}}></div>
+                  }
+                </Col>
+              ))}
+            </Row>
+          ))
+        );
+    };
+
+    return (
+    <div className="container">
+      <div className="storeCell">
+        <div className="storeDescription">
+          <Image style={{height: "200px"}} src={store.image} />
+          <div>
+            <Title level={4}>{store.name}</Title>
+            <div>{store.tag}</div>
+            <div>{store.location}</div>
+          </div>
+        </div>
+      </div>
+
+      <div className="tableGrid">
+        {getTables()}
+        {/* <Button onClick={() => ws.send(JSON.stringify({ 'storeId': storeId, 'tableNum': tableNum, 'command': 'reserve' }))}>예약하기</Button>
+        <Button onClick={() => ws.send(JSON.stringify({ 'storeId': storeId, 'tableNum': tableNum, 'command': 'cancel' }))}>취소하기</Button> */}
+        <Button onClick={() => console.log('예약하기')}>예약하기</Button>
+        <Button onClick={() => console.log('취소하기')}>취소하기</Button>
+      </div>
+
+      <Modal
+        title="인원 선택"
+        open={modalVisible}
+        onCancel={() => setModalVisible(false)}
+        footer={[
+          <Button key="back" onClick={() => setModalVisible(false)}>
+            취소
+          </Button>,
+          <Button
+            key="submit"
+            type="primary"
+            onClick={() =>
+              navigate('menu', {
+                storeId,
+                tableNum,
+                customerNum: adultCount + childCount
+              })
+            }
+          >
+            메뉴 고르기
+          </Button>,
+        ]}
+      >
+        <Row gutter={[10, 10]} align="middle">
+          <Col>
+            <Text>성인</Text>
+            <Button onClick={() => setAdultCount(pre => Math.max(0, pre - 1))}>-</Button>
+            <InputNumber value={adultCount} onChange={value => setAdultCount(value)} min={0} />
+            <Button onClick={() => setAdultCount(pre => pre + 1)}>+</Button>
+          </Col>
+          <Col>
+            <Text>유아</Text>
+            <Button onClick={() => setChildCount(pre => Math.max(0, pre - 1))}>-</Button>
+            <InputNumber value={childCount} onChange={value => setChildCount(value)} min={0} />
+            <Button onClick={() => setChildCount(pre => pre + 1)}>+</Button>
+          </Col>
+        </Row>
+      </Modal>
+    </div>
+  );
+}
---
+++ alleat/src/Store.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Input, Image, Button } from "antd";
+import stores from './stores.js';
+import './Store.css';
+
+export default function StoreList() {
+    const movePage = useNavigate();
+    const [searchText, setSearchText] = useState('');
+    const filteredStores = Object.values(stores).filter((store) =>
+      store.name.toLowerCase().includes(searchText.toLowerCase())
+    );
+
+    return (
+        <div className="Container">
+          <div className="TitleContainer">
+            <div className="TitleName">올잇.</div>
+          </div>
+          <Input 
+            placeholder="가게 이름 검색" 
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
+            style={{ width: '80%', margin: '10px'}}
+            size="large"
+          />
+          <div className="StoreList">
+            {filteredStores.map((store) => (
+              <div className="StoreCell">
+                <div className="StoreDescription">
+                  <Image style={{width: '60px', height: '60px', marginRight: '10px', borderRadius: '7px'}} src={store.image}/>
+                  <div>
+                    <div className="StoreName">{store.name}</div>
+                    <div className="StoreTag">{store.tag}</div>
+                    <div className="StoreLocation">{store.location}</div>
+                  </div>
+                </div>
+                <Button onClick={() => movePage('seat', { state: { storeId: store.id } })} type="primary"
+                className="Button">잔여좌석 확인</Button>
+              </div>
+            ))}
+          </div>
+        </div>
+    );
+}
