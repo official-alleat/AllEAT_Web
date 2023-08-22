f4bab032645cd5cca96e702a9230989e9b4e246e
heeju <heeju@imprimedicine.com>
2023-08-23 00:18:50 +0900

    backend amplify cli 2

---
+++ manager/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "alleat": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
